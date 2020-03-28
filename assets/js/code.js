554483655a2da13fa06951c2243b1dfb0f9fef3c
Hamza Miskini <57097198+H4mz4-mk@users.noreply.github.com>
2020-03-28 16:05:23 +0200

    Update code.js

--- assets/js/code.js
+++ assets/js/code.js
@@ -10,6 +10,10 @@ $('ul').on('click','span',function(event){
 $("input[type='text'").keypress(function(event){
 	if(event.which === 13 ){
 	var toDoText = $(this).val();
+	if( toDoText === "" ){
+		alert("Please enter a valid value!!");
+		stopPropagation();
+	}
 	$('ul').append('<li><span><i class="fa fa-trash"></i> </span>'+ toDoText +'</li>')
 	$(this).val('');
 	}
